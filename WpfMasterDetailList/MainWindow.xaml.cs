f758079969082d5ac2733572e41e95fd93ff3915
Philip Guerrero <guerpc@farmingdale.edu>
2023-03-09 20:59:49 -0500

    Add project files.

---
+++ WpfMasterDetailList/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMasterDetailList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Movie> movies;
+        public MainWindow()
+        {
+            InitializeComponent();
+            movies = new ObservableCollection<Movie>();
+            DataContext = movies;
+            //ListBoxMovies.DataContext = movies;
+            
+        }
+
+        private void ButtonOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamReader sr = new StreamReader("Movies.txt"))
+            {
+                while(!sr.EndOfStream)
+                {
+                    //ObservableCollection<Movie> tempList = new ObservableCollection<Movie>();
+                    Movie m = new Movie();
+                    m.Name = sr.ReadLine();
+                    m.RottenTomatosScore = Convert.ToInt32(sr.ReadLine());
+                    m.Review= sr.ReadLine();
+                    movies.Add(m);
+
+                }
+
+            }
+
+        }
+
+
+        /*private void OpenFile(object target, ExecutedRoutedEventArgs e)
+        {
+
+        }*/
+    }
+}
---
+++ WpfMasterDetailList/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMasterDetailList
+{
+    public class Movie
+    {
+       public string Name { get; set; }
+       public string Review { get; set; }
+       public int RottenTomatosScore { get; set; }
+
+
+        public Movie()
+        {
+            Name = "No Movie Name";
+            Review = "No Review";
+            RottenTomatosScore = 0;
+
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
